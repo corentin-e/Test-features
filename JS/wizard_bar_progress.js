53ad3475d8467544f80f82747c86d0c5a23a0bc7
Etha <76168237+etharion29@users.noreply.github.com>
2022-11-30 15:51:54 +0100

    change variable name of progress bar

--- JS/wizard_bar_progress.js
+++ JS/wizard_bar_progress.js
@@ -1,33 +1,33 @@
 const stepsWizzard = document.getElementsByClassName('button-step-wizard');
-const elem = document.getElementById('myBar');
+const progessBar = document.getElementById('myBar');
 const buttonStep = document.getElementById('step_button');
 
 
 function stepWizard1() {
-    elem.style.width = 25 + "%";
+    progessBar.style.width = 25 + "%";
     buttonStep.innerHTML = 'step 2'
 }
 
 function stepWizard2() {
-    elem.style.width = 50 + "%";
+    progessBar.style.width = 50 + "%";
     buttonStep.innerHTML = 'step 3'
 }
 
 function stepWizard3() {
-    elem.style.width = 100 + "%";
+    progessBar.style.width = 100 + "%";
     buttonStep.innerHTML = 'Terminé'
 }
 
 
 function nextButton() {
     if (buttonStep.innerHTML === "step 1") {
-        elem.style.width = 25 + "%";
+        progessBar.style.width = 25 + "%";
         buttonStep.innerHTML = 'step 2'
     } else if (buttonStep.innerHTML === "step 2") {
-        elem.style.width = 50 + "%";
+        progessBar.style.width = 50 + "%";
         buttonStep.innerHTML = 'step 3'
     } else if (buttonStep.innerHTML === "step 3") {
-        elem.style.width = 100 + "%";
+        progessBar.style.width = 100 + "%";
         buttonStep.innerHTML = 'Terminé'
     } 
 }
